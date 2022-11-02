aeac6b090e0fc2063c5ed31f81ea298f912ee0a0
rickyputrasardika22 <rickyputrasardika22@gmail.com>
2022-11-02 15:11:05 +0700

    UTS_PAB

---
+++ app/src/main/java/com/uts_pab/MainActivity.java
@@ -0,0 +1,36 @@
+package com.uts_pab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Spinner spinner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        spinner = findViewById(R.id.spinner);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getApplicationContext(), R.array.jalur_pendaftaran, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getApplicationContext(),adapter.getItem(i), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void OnNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+    }
+}
