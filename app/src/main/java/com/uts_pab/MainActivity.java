86ff46c4a3412325fad7375324ee8241746536c2
rickyputrasardika22 <rickyputrasardika22@gmail.com>
2022-11-02 16:55:22 +0700

    UTS_PAB_Ricky

--- app/src/main/java/com/uts_pab/MainActivity.java
+++ app/src/main/java/com/uts_pab/MainActivity.java
@@ -2,42 +2,62 @@ package com.uts_pab;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-import java.util.ArrayList;
-
 public class MainActivity extends AppCompatActivity {
-    private Spinner spinner;
-    private ArrayList<String> arrayJalurPendaftaran;
+//deklarasi objek
+    EditText etNama;
+    EditText etNomor;
+    Spinner spJalurPendaftaran;
+    Button btPindah;
+    CheckBox centang;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        spinner = findViewById(R.id.spinner);
+        etNama = findViewById(R.id.et_nama);
+        etNomor = findViewById(R.id.et_nomor);
+        spJalurPendaftaran = findViewById(R.id.spinner);
+        btPindah = findViewById(R.id.btn_pindah);
+        centang = findViewById(R.id.check);
 
-        arrayJalurPendaftaran = new ArrayList<>();
-        arrayJalurPendaftaran.add("Tes Tertulis");
-        arrayJalurPendaftaran.add("Jalur Tanpa Tes");
-        arrayJalurPendaftaran.add("Ujian Saringan Masuk");
-
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getApplicationContext(), R.array.jalur_pendaftaran, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
-        spinner.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        btPindah.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(getApplicationContext(),adapter.getItem(i), Toast.LENGTH_SHORT).show();
-            }
+            public void onClick(View view) {
 
-            @Override
-            public void OnNothingSelected(AdapterView<?> adapterView) {
+                String namaPeserta = etNama.getText().toString();
+                String nomorPendaftaran = etNomor.getText().toString();
 
+                String jalur = spJalurPendaftaran.getSelectedItem().toString();
+
+                if(namaPeserta.equals(""))
+                {
+                    etNama.setError("Nama Kosong Bre!!!");
+                }
+                else if(nomorPendaftaran.equals(""))
+                {
+                    etNomor.setError("Nomor Kosong Ni Gan!!!!");
+                }
+                else if(!centang.isChecked())
+                {
+                    Toast.makeText(MainActivity.this, "Harus Diconteng", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Intent pindah = new Intent(MainActivity.this, MainActivity2.class);
+                    pindah.putExtra("xNama", namaPeserta);
+                    pindah.putExtra("xNomor", nomorPendaftaran);
+                    pindah.putExtra("xJalur", jalur);
+                    startActivity(pindah);
+                }
             }
         });
     }
--- app/src/main/java/com/uts_pab/MainActivity2.java
+++ app/src/main/java/com/uts_pab/MainActivity2.java
@@ -2,13 +2,31 @@ package com.uts_pab;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    TextView tvnamaPeserta, tvnomorPeserta, tvjalur;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        tvnamaPeserta = findViewById(R.id.tv_output_nama);
+        tvnomorPeserta = findViewById(R.id.tv_output_nomor);
+        tvjalur = findViewById(R.id.tv_output_jalur_pendaftaran);
+
+        Intent terima = getIntent();
+        String yNama = terima.getStringExtra("xNama");
+        String yNomor = terima.getStringExtra("xNomor");
+        String yJalur = terima.getStringExtra("xJalur");
+
+        tvnamaPeserta.setText(yNama);
+        tvnomorPeserta.setText(yNomor);
+        tvjalur.setText(yJalur);
+
     }
 }
