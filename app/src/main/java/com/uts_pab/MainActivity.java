415c7be204deb20100e57291ace3a8d8e56bb5d0
rickyputrasardika22 <rickyputrasardika22@gmail.com>
2022-11-02 15:27:01 +0700

    Update MainActivity.java

--- app/src/main/java/com/uts_pab/MainActivity.java
+++ app/src/main/java/com/uts_pab/MainActivity.java
@@ -9,14 +9,22 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     private Spinner spinner;
+    private ArrayList<String> arrayJalurPendaftaran;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         spinner = findViewById(R.id.spinner);
+
+        arrayJalurPendaftaran = new ArrayList<>();
+        arrayJalurPendaftaran.add("Tes Tertulis");
+        arrayJalurPendaftaran.add("Jalur Tanpa Tes");
+        arrayJalurPendaftaran.add("Ujian Saringan Masuk");
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getApplicationContext(), R.array.jalur_pendaftaran, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
